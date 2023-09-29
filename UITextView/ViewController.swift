a20ddf3d8038acdb23fb3040f03aaf04488b7fa3
user234888 <user234888@NY551.local>
2023-09-29 10:20:05 -0400

    Adds functionality for clear and submit buttons

--- UITextView/ViewController.swift
+++ UITextView/ViewController.swift
@@ -11,13 +11,13 @@ import UIKit
 class ViewController: UIViewController {
 
 
-    
-    @IBOutlet weak var summary: UITextView!
     @IBOutlet weak var firstName: UITextField!
     @IBOutlet weak var lastName: UITextField!
     @IBOutlet weak var country: UITextField!
     @IBOutlet weak var age: UITextField!
-    @IBOutlet weak var fistNameErrorLabel: UILabel!
+    @IBOutlet weak var summary: UITextView!
+    @IBOutlet weak var errorMessage: UILabel!
+    @IBOutlet weak var successMessage: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -25,25 +25,70 @@ class ViewController: UIViewController {
         lastName.placeholder = "Last Name"
         country.placeholder = "Country"
         age.placeholder = "Age"
-        summary.text = ""
-        
+        //summary.text = ""
+        setLabelMessages()
+        errorMessage.isHidden = true
+        successMessage.isHidden = true
+                
     }
     
-    
-    
-    
+    func setLabelMessages(){
+        errorMessage.text = "Complete the missing info"
+        successMessage.text = "Successfully submitted!"
+    }
     @IBAction func addDetails(_ sender: Any) {
-        if validateFields()
-            updateSummary()
+        validateFields()
+        updateSummary()
+    }
+    @IBAction func clearFields(_ sender: Any) {
+        clearAllFields()
+        successMessage.isHidden = true
+        errorMessage.isHidden = true
+    }
+    @IBAction func submitForm(_ sender: Any) {
+        if summary.text.isEmpty {
+                    errorMessage.isHidden = false
+                    successMessage.isHidden = true
+                }
+                else {
+                    successMessage.isHidden = false
+                    errorMessage.isHidden = true
+                }
     }
     func updateSummary(){
-        summary.text = "Summary: \n\t \(firstName.text) \(lastName.text)\n\t \(country.text)\n\t\(age.text)"
+       let myText = "Summary: \n\t \(firstName.text) \(lastName.text)\n\t \(country.text)\n\t\(age.text)"
+        summary.text = myText;
+        clearAllFieldsExceptSummary()
+        successMessage.isHidden = true
+        errorMessage.isHidden = true
+        
     }
+    func clearAllFields(){
+       clearAllFieldsExceptSummary()
+        summary.text?.removeAll()
+    }
+    func clearAllFieldsExceptSummary(){
+        firstName.text?.removeAll()
+        lastName.text?.removeAll()
+        age.text?.removeAll()
+        country.text?.removeAll()
+    }
+    
     func validateFields(){
-        if firstName.text.isEmpty{
-            fistNameErrorLabel.setNeedsDisplay()
+        guard let fName = firstName.text,
+              let lName = lastName.text,
+              let country_name = country.text,
+              let uAge = age.text,
+              fName.isEmpty, !lName.isEmpty, !country_name.isEmpty, !uAge.isEmpty,
+              let ageVal = Int(uAge)
+        else {
+            errorMessage.isHidden = false
+            successMessage.isHidden = true
+            return
         }
+        
     }
+            
 }
 
 	
